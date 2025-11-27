1b69fd2246436a5adc2d494acb62bd8c11c014ce
lephantrunghieu <lephantrunghieu555@gmail.com>
2025-11-28 00:06:05 +0700

    add

---
+++ webLaptop/src/main/java/com/lpth/webLaptop/WebLaptopApplication.java
@@ -0,0 +1,13 @@
+package com.lpth.webLaptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLaptopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLaptopApplication.class, args);
+	}
+
+}
---
+++ webLaptop/src/test/java/com/lpth/webLaptop/WebLaptopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpth.webLaptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLaptopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
